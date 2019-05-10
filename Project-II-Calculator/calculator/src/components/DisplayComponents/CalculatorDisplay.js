70a823ce1a69bf90148f7a45d123d02bdc90c3ab
Kyle Barnett <kbarnett23@gmail.com>
2019-05-09 21:03:59 -0600

    started calculator

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+import ActionButton from '../ButtonComponents/ActionButton'
+
+const CalculatorDisplay = (props) => {
+  return (
+    <div>
+      <h1>{props.message.something}</h1>
+    </div>
+  )
+}
+
+export default CalculatorDisplay
